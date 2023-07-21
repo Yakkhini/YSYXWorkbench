31ae8e394e603bd61d7542b4111be87330198d8e
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2023-07-21 20:11:17 +0800

    Feat: Prepare shell env for compile nvboard.

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
   in {
     formatter.x86_64-linux = pkgs.legacyPackages.x86_64-linux.alejandra;
     devShells.x86_64-linux.default = pkgs.legacyPackages.x86_64-linux.mkShell {
+      name = "ysyx-shell";
       packages = [
         verilator5002pkgs.legacyPackages.x86_64-linux.verilator
         stdpkgs.gtkwave
@@ -22,11 +23,22 @@
         stdpkgs.readline
         stdpkgs.llvm
         stdpkgs.gdb
+        stdpkgs.python3
       ];
+
+      buildInputs = with stdpkgs; [
+        gnumake
+        SDL2
+        SDL2_image
+        pkg-config
+      ];
+
       shellHook = ''
         export NPC_HOME=`readlink -f npc`
         export NEMU_HOME=`readlink -f nemu`
         export AM_HOME=`readlink -f abstract-machine`
+        export NVBOARD_HOME=`readlink -f nvboard`
+        NIX_CFLAGS_COMPILE="$(pkg-config --cflags sdl2) $NIX_CFLAGS_COMPILE"
       '';
     };
   };
