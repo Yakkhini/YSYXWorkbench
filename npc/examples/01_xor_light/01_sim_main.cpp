ae8916c3f2f53b255fe08ea37b820c1764decee1
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2023-07-30 17:07:44 +0800

    Feat: YSYX showcase xor light with NVBoard.

---
+++ npc/examples/01_xor_light/01_sim_main.cpp
@@ -0,0 +1,22 @@
+#include <nvboard.h>
+#include <Vxormod.h>
+#include "verilated.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(int argc, char** argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vxormod* top = new Vxormod{contextp};
+    nvboard_bind_pin( &top->a, BIND_RATE_SCR, BIND_DIR_IN , 1, SW0);
+    nvboard_bind_pin( &top->b, BIND_RATE_SCR, BIND_DIR_IN , 1, SW1);
+    nvboard_bind_pin( &top->f, BIND_RATE_SCR, BIND_DIR_OUT, 1, LD0);    
+    nvboard_init();
+    while (true) {
+        nvboard_update();
+        top->eval();
+    }
+
+    nvboard_quit();
+}
---
+++ npc/examples/01_xor_light/xormod.v
@@ -0,0 +1,7 @@
+module xormod(
+  input a,
+  input b,
+  output f
+);
+  assign f = a ^ b;
+endmodule
