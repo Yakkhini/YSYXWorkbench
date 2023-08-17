96346b9793171f6fb4f6254ca1db2e3b71a35e5c
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2023-08-17 22:47:54 +0800

    Feat(nemu): Implement the expression generate tool.

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,38 +1,95 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
+#include <assert.h>
+#include <ctype.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <assert.h>
 #include <string.h>
+#include <time.h>
 
 // this should be enough
 static char buf[65536] = {};
+static int buf_pos = 0;
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
-static char *code_format =
-"#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
-"}";
+static char *code_format = "#include <stdio.h>\n"
+                           "int main() { "
+                           "  unsigned result = %s; "
+                           "  printf(\"%%u\", result); "
+                           "  return 0; "
+                           "}";
+
+static void gen(char c) {
+  int j = rand() % 2;
+  if (j == 0 && isdigit(c) == 0) {
+    gen(' ');
+  };
+  char *read_pos = buf + buf_pos;
+  *read_pos = c;
+  buf_pos++;
+}
+
+static void gen_rand_op() {
+  int i = rand() % 4;
+  switch (i) {
+  case 0:
+    gen('+');
+    break;
+  case 1:
+    gen('-');
+    break;
+  case 2:
+    gen('*');
+    break;
+  case 3:
+    gen('/');
+    break;
+  }
+}
+
+static void gen_num() {
+  int first_num = rand() % 9 + 1;
+  gen(first_num + '0');
+  int num_digit = rand() % 7 + 1;
+  for (int i = 0; i < num_digit; i++) {
+    int num = rand() % 10;
+    gen(num + '0');
+  }
+}
 
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  int i = rand() % 3;
+  if (buf_pos > 16) {
+    i = rand() % 2;
+  }
+  switch (i) {
+  case 0:
+    gen_num();
+    break;
+  case 1:
+    gen('(');
+    gen_rand_expr();
+    gen(')');
+    break;
+  default:
+    gen_rand_expr();
+    gen_rand_op();
+    gen_rand_expr();
+    break;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -43,8 +100,10 @@ int main(int argc, char *argv[]) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
-  for (i = 0; i < loop; i ++) {
+  for (i = 0; i < loop; i++) {
     gen_rand_expr();
+    gen('\0');
+    buf_pos = 0;
 
     sprintf(code_buf, code_format, buf);
 
@@ -53,8 +112,9 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    int ret = system("gcc -w /tmp/.code.c -o /tmp/.expr");
+    if (ret != 0)
+      continue;
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
