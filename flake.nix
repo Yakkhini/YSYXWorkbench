f1526628704a09e83e23e4530063f709dbb01013
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2023-08-17 11:10:14 +0800

    Flake: ista-bin package.

--- flake.nix
+++ flake.nix
@@ -2,17 +2,54 @@
   description = "Flake for ysyx";
   inputs = {
     verilator5008pkgs.url = "github:NixOS/nixpkgs/5e871533c4a488319b9cb98d7a525c356459d36c";
+    yamlcpp06pkgs.url = "github:NixOS/nixpkgs/66e44425c6dfecbea68a5d6dc221ccd56561d4f1";
     pkgs.url = "pkgs";
   };
   outputs = {
     self,
     pkgs,
     verilator5008pkgs,
+    yamlcpp06pkgs,
   }: let
     stdpkgs = pkgs.legacyPackages.x86_64-linux;
     npcmake = stdpkgs.writeScriptBin "npcmake" ''make -C $NPC_HOME $1'';
-  in {
+  in rec {
     formatter.x86_64-linux = pkgs.legacyPackages.x86_64-linux.alejandra;
+    packages.x86_64-linux.ista-bin = pkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
+      name = "ista-bin";
+
+      system = "x86_64-linux";
+
+      src = ./npc/bin/iSTA;
+      nativeBuildInputs = [
+        stdpkgs.autoPatchelfHook # Automatically setup the loader, and do the magic
+      ];
+
+      # Required at running time
+      buildInputs = [
+        stdpkgs.glibc
+        stdpkgs.libunwind
+        stdpkgs.zlib
+        stdpkgs.tcllib
+        yamlcpp06pkgs.legacyPackages.x86_64-linux.libyamlcpp
+      ];
+
+      unpackPhase = "true";
+
+      # Extract and copy executable in $out/bin
+      installPhase = ''
+        mkdir -p $out/bin
+        cp $src $out/bin/ista-bin
+      '';
+
+      meta = with stdpkgs.lib; {
+        description = "iSTA binary";
+        homepage = https://github.com/OSCC-Project/iEDA;
+        license = licenses.mulan-psl2;
+        maintainers = with stdenv.lib.maintainers; [YAKKHINI];
+        platforms = ["x86_64-linux"];
+      };
+    };
     devShells.x86_64-linux.default = pkgs.legacyPackages.x86_64-linux.mkShell {
       name = "ysyx-shell";
       packages = [
@@ -25,6 +62,9 @@
         stdpkgs.llvm
         stdpkgs.gdb
         stdpkgs.python3
+        stdpkgs.perl
+        stdpkgs.yosys
+        packages.x86_64-linux.ista-bin
         npcmake
       ];
 
