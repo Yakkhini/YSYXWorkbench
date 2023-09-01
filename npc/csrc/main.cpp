52caab5d2fb7da0535b5eae8c47a52b9f2973612
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2023-09-01 17:15:36 +0800

    Feat(npc): Mem data & wave trace in cpp code.

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,9 +1,18 @@
 #include <Vsriz.h>
+#include <Vsriz__Dpi.h>
 #include <cstdint>
 #include <stdio.h>
 #include <verilated.h>
+#include <verilated_vcd_c.h>
 
-uint32_t pmem_read(IData x) { return 0; }
+static uint32_t mem[32];
+
+int pmem_read(int pc) {
+  printf("Enter pmem_read function.");
+  return 0;
+}
+
+void mem_init() {}
 
 int main(int argc, char **argv) {
   VerilatedContext *contextp = new VerilatedContext;
@@ -12,9 +21,29 @@ int main(int argc, char **argv) {
   Vsriz *top = new Vsriz(contextp);
   printf("Hello, SuanChou Processor Core!\n");
 
-  while (!contextp->gotFinish()) {
-    top->inst = pmem_read(top->pc);
+  mem_init();
+
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *tfp = new VerilatedVcdC;
+  top->trace(tfp, 5);
+  tfp->open("waveform.vcd");
+  int sim_time = 30000000;
+
+  while (contextp->time() < sim_time && !contextp->gotFinish()) {
+    contextp->timeInc(1);
+    top->clk = 0;
     top->eval();
+    tfp->dump(contextp->time());
+    contextp->timeInc(1);
+    top->clk = 1;
+    top->eval();
+    tfp->dump(contextp->time());
   }
+
+  tfp->close();
+
+  top->final();
+  delete top;
+
   return 0;
 }
