b98ede637aa502092c7599b77e54eac7c02cffa0
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2023-09-10 16:34:33 +0800

    Feat(am): Implement strcmp, strcpy, strcat, memset, memcmp functions in klib and pass string test.

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,15 +1,21 @@
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  panic("Not implemented");
-}
+size_t strlen(const char *s) { panic("Not implemented"); }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  int i = 0;
+  while (src[i] != '\0') {
+    dst[i] = src[i];
+    i++;
+  }
+
+  dst[i] = '\0';
+
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +23,49 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  int i = 0;
+  int j = 0;
+  while (dst[i] != '\0') {
+    i++;
+  }
+
+  while (src[j] != '\0') {
+    dst[i] = src[j];
+    i++;
+    j++;
+  }
+
+  dst[i] = '\0';
+
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int i = 0;
+  int result = 0;
+
+  while (true) {
+    if (s1[i] == s2[i]) {
+      i++;
+
+      if (s1[i] == '\0' || s2[i] == '\0') {
+        break;
+      }
+      continue;
+    } else if (s1[i] < s2[i]) {
+      result = -1;
+      break;
+    } else {
+      result = 1;
+      break;
+    }
+
+    if (s1[i] == '\0' || s2[i] == '\0') {
+      break;
+    }
+  }
+
+  return result;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,19 +73,48 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  int *dst = s;
+  for (int i = 0; i < n; i++) {
+    dst[i] = c;
+  }
+
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
-}
+void *memcpy(void *out, const void *in, size_t n) { panic("Not implemented"); }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  int i = 0;
+  int result = 0;
+  const char *s1_char = s1;
+  const unsigned char *s2_char = s2;
+
+  while (true) {
+    if (s1_char[i] == s2_char[i]) {
+      i++;
+
+      if (s1_char[i] == '\0' || s2_char[i] == '\0') {
+        break;
+      }
+      continue;
+    } else if (s1_char[i] < s2_char[i]) {
+      result = -1;
+      break;
+    } else {
+      result = 1;
+      break;
+    }
+
+    if (s1_char[i] == '\0' || s2_char[i] == '\0') {
+      break;
+    }
+  }
+
+  return result;
 }
 
 #endif
