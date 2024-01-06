6ea941c0b676748f9aa42759d3c2a1e1865e6af6
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-01-06 21:20:26 +0800

    Feat(nemu): Store functions info to a link table.

--- nemu/src/cpu/ftrace.c
+++ nemu/src/cpu/ftrace.c
@@ -7,7 +7,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct FunctionNode {
+  char *name;
+  vaddr_t pos;
+  struct FunctionNode *next;
+} FunctionNode;
+
 static int ftrace_is_enabled = false;
+static FunctionNode *head = NULL;
+
+void function_link_table_insert(char *name, vaddr_t pos);
+void function_link_table_print();
 
 void enable_ftrace(char *path) {
   ftrace_is_enabled = true; // Maybe depends on TRACE or other options?
@@ -56,12 +66,47 @@ void enable_ftrace(char *path) {
   assert(check == 1);
 
   for (int i = 0; i < symnum; i++) {
-    if (ELF32_ST_TYPE(syms[i].st_info) == STT_FUNC) {
-      printf("Found function symbol: %s.\n", strtab + syms[i].st_name);
+    if (ELF32_ST_TYPE(syms[i].st_info) != STT_FUNC) {
+      continue;
     }
+
+    function_link_table_insert(strtab + syms[i].st_name, syms[i].st_value);
   }
+
+  function_link_table_print();
 
   fclose(elf_file);
 
   return;
+}
+
+void function_link_table_insert(char *name, vaddr_t pos) {
+
+  if (head == NULL) {
+    head = (FunctionNode *)malloc(sizeof(FunctionNode));
+    head->name = name;
+    head->pos = pos;
+    head->next = NULL;
+    return;
+  }
+
+  FunctionNode *current = head;
+  while (current->next != NULL) {
+    current = current->next;
+  }
+
+  current->next = (FunctionNode *)malloc(sizeof(FunctionNode));
+  current->next->name = name;
+  current->next->pos = pos;
+  current->next->next = NULL;
+
+  return;
+}
+
+void function_link_table_print() {
+  FunctionNode *current = head;
+  while (current != NULL) {
+    printf("Function name in link table: %s\n", current->name);
+    current = current->next;
+  }
 }
