68ee27150a6a1adec0d8deb2c561a823e2a5f5a8
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-01-22 10:59:40 +0800

    Refactor(npc): Move functions to monitor.

---
+++ npc/csrc/monitor/monitor.cpp
@@ -0,0 +1,59 @@
+#include <common.h>
+#include <getopt.h>
+#include <memory/paddr.h>
+
+static char *NPC_HOME = getenv("NPC_HOME");
+static char *img_file = NULL;
+
+static uint32_t DEFAULT_MEM[] = {
+    0x3e800093, 0x3e810093, 0x3e810193, 0x7d018213, 0x3e820293,
+    0x3e828313, 0x7d008113, 0x00430313, 0x3e800093, 0x3e810093,
+    0x3e810193, 0x7d018213, 0x3e820293, 0x3e828313, 0x7d008113,
+    0x00430313, 0x3e800093, 0x3e810093, 0x3e810193, 0x7d018213,
+    0x3e820293, 0x3e828313, 0x7d008113, 0x00430313, 0x00100073};
+
+void load_img() {
+  if (img_file == NULL) {
+    printf("No image is given. Use the default build-in image.");
+    memcpy(guest_to_host(0x80000000), DEFAULT_MEM, sizeof(uint32_t) * 25);
+    return; // built-in image size
+  }
+
+  FILE *fp = fopen(img_file, "rb");
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  printf("The image is %s, size = %ld\n", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(0x80000000), size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return;
+}
+
+void parse_args(int argc, char *argv[]) {
+  const struct option table[] = {
+      {0, 0, NULL, 0},
+  };
+  int o;
+  while ((o = getopt_long(argc, argv, "-h:", table, NULL)) != -1) {
+    switch (o) {
+    case 1:
+      img_file = optarg;
+      return;
+    default:
+      printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+      printf("\t-b,--batch              run with batch mode\n");
+      printf("\t-f,--ftrace=FILE        trace functions with elf file.\n");
+      printf("\t-l,--log=FILE           output log to FILE\n");
+      printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+      printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+      printf("\n");
+      exit(0);
+    }
+  }
+  return;
+}
---
+++ npc/include/monitor.h
@@ -0,0 +1,7 @@
+#ifndef __MONITOR_H__
+#define __MONITOR_H__
+
+void load_img();
+void parse_args(int argc, char *argv[]);
+
+#endif
