6b173a24f3e65f62ea94a3e8e3ebf2f34c4891be
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-01-31 22:43:50 +0800

    Feat(am) Timer uptime.

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,20 @@
 #include <am.h>
 #include <nemu.h>
 
-void __am_timer_init() {
-}
+static AM_TIMER_UPTIME_T uptime_reg;
+
+void __am_timer_init() { uptime_reg.us = 0; }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uptime->us = uptime_reg.us;
+  uptime_reg.us++;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->hour = 0;
+  rtc->day = 0;
+  rtc->month = 0;
+  rtc->year = 1900;
 }
