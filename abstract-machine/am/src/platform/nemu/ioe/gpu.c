2035fcf4b8a4795dbe91431a6df90ec23528cfd0
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-02-19 06:57:22 +0000

    Fix(am): Only outl allowed in GPU.

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,14 +4,12 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 uint32_t screen_w;
 uint32_t screen_h;
-uint32_t *fb = NULL;
 
 void __am_gpu_init() {
   // ref: vgactl_port_base[0] = (screen_width() << 16) | screen_height();
   uint32_t size = inl(VGACTL_ADDR);
   screen_w = size >> 16;
   screen_h = size & 0xffff;
-  fb = (uint32_t *)(uintptr_t)FB_ADDR;
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -35,7 +33,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   for (i = 0; i < h && y + i < H; i++) {
     int j;
     for (j = 0; j < w && x + j < W; j++) {
-      fb[(y + i) * W + x + j] = px[i * w + j];
+      outl(FB_ADDR + ((y + i) * W + x + j) * 4, px[i * w + j]);
     }
   }
 }
