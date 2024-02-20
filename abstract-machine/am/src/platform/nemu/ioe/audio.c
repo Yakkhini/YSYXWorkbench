f1280260596688b109102595778958b83fec929f
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-02-20 09:54:44 +0000

    Feat(am, nemu): Audio support via SDL.

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,26 +1,46 @@
 #include <am.h>
 #include <nemu.h>
 
-#define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
-#define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
-#define AUDIO_SAMPLES_ADDR   (AUDIO_ADDR + 0x08)
+#define AUDIO_FREQ_ADDR (AUDIO_ADDR + 0x00)
+#define AUDIO_CHANNELS_ADDR (AUDIO_ADDR + 0x04)
+#define AUDIO_SAMPLES_ADDR (AUDIO_ADDR + 0x08)
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
-#define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
-#define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
+#define AUDIO_INIT_ADDR (AUDIO_ADDR + 0x10)
+#define AUDIO_COUNT_ADDR (AUDIO_ADDR + 0x14)
+
+static uint32_t sbuf_offset; // For write
+static uint32_t sbuf_size;
 
 void __am_audio_init() {
+  sbuf_offset = 0;
+  sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = sbuf_size;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  outl(AUDIO_INIT_ADDR, 1);
+  outl(AUDIO_COUNT_ADDR, 0);
 }
 
-void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
-}
+void __am_audio_status(AM_AUDIO_STATUS_T *stat) { stat->count = 1; }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int len = ctl->buf.end - ctl->buf.start;
+  uint8_t *bf = (uint8_t *)ctl->buf.start;
+  int count = inl(AUDIO_COUNT_ADDR);
+  int i;
+  for (i = 0; i < len; i++) {
+    outb(AUDIO_SBUF_ADDR + sbuf_offset, bf[i]);
+    sbuf_offset++;
+    sbuf_offset %= sbuf_size;
+  }
+
+  outl(AUDIO_COUNT_ADDR, count + len);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -13,9 +13,9 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include <SDL2/SDL.h>
 #include <common.h>
 #include <device/map.h>
-#include <SDL2/SDL.h>
 
 enum {
   reg_freq,
@@ -28,20 +28,59 @@ enum {
 };
 
 static uint8_t *sbuf = NULL;
+static uint32_t sbuf_offset; // For read
 static uint32_t *audio_base = NULL;
 
+static void audio_play(void *userdata, uint8_t *stream, int len) {
+  int nread = len;
+  if (audio_base[reg_count] < len)
+    nread = audio_base[reg_count];
+  for (int i = 0; i < nread; i++) {
+    stream[i] = sbuf[sbuf_offset];
+    sbuf_offset++;
+    sbuf_offset %= CONFIG_SB_SIZE;
+    audio_base[reg_count]--;
+  }
+  if (len > nread) {
+    memset(stream + nread, 0, len - nread);
+  }
+}
+
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  if (!is_write || !audio_base[reg_init]) {
+    return;
+  }
+
+  SDL_AudioSpec spec = {};
+  spec.freq = audio_base[reg_freq];
+  spec.format = AUDIO_S16SYS;
+  spec.channels = audio_base[reg_channels];
+  spec.samples = audio_base[reg_samples];
+  spec.callback = audio_play;
+  spec.userdata = NULL;
+
+  int ret = SDL_InitSubSystem(
+      SDL_INIT_AUDIO); // SDL_Init in VGA so audio should be initialized later
+  if (ret == 0) {
+    SDL_OpenAudio(&spec, NULL);
+    SDL_PauseAudioDevice(1, 0);
+  }
 }
 
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
 #ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
+  add_pio_map("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size,
+              audio_io_handler);
 #else
-  add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
+  add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size,
+               audio_io_handler);
 #endif
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+
+  sbuf_offset = 0;
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
 }
