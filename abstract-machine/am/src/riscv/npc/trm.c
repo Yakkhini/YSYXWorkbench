3138a61ec4587685a746a7222973018600f20eae
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-02-21 02:21:58 +0000

    Feat(am, npc): Basic serial device support to pass hello.

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
+#include <npc.h>
 
 extern char _heap_start;
 int main(const char *args);
@@ -14,11 +15,12 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 #endif
 static const char mainargs[] = MAINARGS;
 
-void putch(char ch) {}
+void putch(char ch) { outb(SERIAL_PORT, ch); }
 
 void halt(int code) {
-  asm volatile("mv a0, %0; ebreak" : : "r"(code));
-  while (1);
+  npc_trap(code);
+  while (1)
+    ;
 }
 
 void _trm_init() {
--- npc/csrc/device/device.cpp
+++ npc/csrc/device/device.cpp
@@ -3,4 +3,14 @@
 
 word_t mmio_read(paddr_t addr, int len) { return 0; }
 
-void mmio_write(paddr_t addr, int len, word_t data) { return; }
+void mmio_write(paddr_t addr, int len, word_t data) {
+  switch (addr) {
+  case CONFIG_SERIAL_MMIO:
+    putchar((char)(data & 0xff));
+    break;
+  default:
+    break;
+  }
+
+  return;
+}
