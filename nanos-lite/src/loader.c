04fff31bb7a0c359624929e6e3980588a35f3361
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-03 03:53:38 +0000

    Feat(nanos): Loader support to load user program.

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <stdint.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,9 +10,30 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr *ehdr = (Elf_Ehdr *)malloc(sizeof(Elf_Ehdr));
+  ramdisk_read(ehdr, 0, sizeof(Elf_Ehdr));
+  Elf_Phdr *phdr_list = (Elf_Phdr *)malloc(ehdr->e_phnum * sizeof(Elf_Phdr));
+  ramdisk_read(phdr_list, ehdr->e_phoff, ehdr->e_phnum * sizeof(Elf_Phdr));
+
+  for (int i = 0; i < ehdr->e_phnum; i++) {
+    if (phdr_list[i].p_type == PT_LOAD) {
+      uintptr_t addr = phdr_list[i].p_vaddr;
+      uintptr_t off = phdr_list[i].p_offset;
+      uintptr_t filesz = phdr_list[i].p_filesz;
+      uintptr_t memsz = phdr_list[i].p_memsz;
+      void *buf = malloc(filesz);
+      Log("Loading [0x%08x, 0x%08x) to [0x%08x, 0x%08x)",
+          off, off + filesz, addr, addr + memsz);
+      ramdisk_read(buf, off, filesz);
+      memcpy((void *)addr, buf, filesz);
+      memset((void *)(addr + filesz), 0, memsz - filesz);
+    }
+  }
+  return ehdr->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
