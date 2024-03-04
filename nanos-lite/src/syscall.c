68809dacd94ecd4d84f6ed150fed5fce48b2f184
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-04 12:19:54 +0000

    Refactor(nanos): Make varible names & log function more friendly.

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,26 +9,28 @@ void do_syscall(Context *c) {
       "SYS_link",  "SYS_unlink", "SYS_wait",   "SYS_times",  "SYS_gettimeofday",
   };
 
-  uintptr_t a[5];
-  a[0] = c->GPR1;
-  a[1] = c->GPR2;
-  a[2] = c->GPR3;
-  a[3] = c->GPR4;
-  a[4] = c->GPRx;
+  uintptr_t type = c->GPR1;
+  uintptr_t ret = c->GPRx;
 
-  switch (a[0]) {
+  uintptr_t a[3];
+  a[0] = c->GPR2;
+  a[1] = c->GPR3;
+  a[2] = c->GPR4;
+
+  Log("Handling syscall ID = %d (%s), arguments = %d, %d, %d, %d", type,
+      syscall_names[type], a[0], a[1], a[2]);
+
+  switch (type) {
   case SYS_exit:
-    halt(a[4]);
+    halt(a[0]);
     break;
   case SYS_yield:
     yield();
-    a[4] = 0;
+    ret = 0;
     break;
   default:
-    panic("Unhandled syscall ID = %d", a[0]);
+    panic("Unhandled syscall ID = %d", type);
   }
 
-  Log("Handling syscall ID = %d (%s), arguments = %d, %d, %d, %d, "
-      "return value = %d.",
-      a[0], syscall_names[a[0]], a[1], a[2], a[3], a[4]);
+  Log("Return value = %d", ret);
 }
