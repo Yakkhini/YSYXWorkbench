f219f1d65c1b1d9aa7a30166d402cbb02ac9d51b
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-04 07:43:42 +0000

    Feat(nanos): Yield syscall support.

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,15 @@
 #include <common.h>
 #include "syscall.h"
 void do_syscall(Context *c) {
-  uintptr_t a[4];
+  uintptr_t a[5];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+  a[4] = c->GPRx;
 
   switch (a[0]) {
+    case SYS_yield: Log("SYS_yield"); yield(); a[4] = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
