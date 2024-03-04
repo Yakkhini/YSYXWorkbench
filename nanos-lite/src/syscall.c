0ee37f47ac502b4200205d4ccd6b1bfa78b757b4
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-04 09:03:38 +0000

    Feat(nanos): Strace.

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,14 @@
 #include "syscall.h"
 #include <common.h>
+
 void do_syscall(Context *c) {
+  char *syscall_names[] = {
+      "SYS_exit",  "SYS_yield",  "SYS_open",   "SYS_read",   "SYS_write",
+      "SYS_kill",  "SYS_getpid", "SYS_close",  "SYS_lseek",  "SYS_brk",
+      "SYS_fstat", "SYS_time",   "SYS_signal", "SYS_execve", "SYS_fork",
+      "SYS_link",  "SYS_unlink", "SYS_wait",   "SYS_times",  "SYS_gettimeofday",
+  };
+
   uintptr_t a[5];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
@@ -10,15 +18,17 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
   case SYS_exit:
-    Log("SYS_exit");
     halt(0);
     break;
   case SYS_yield:
-    Log("SYS_yield");
     yield();
     a[4] = 0;
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  Log("Handling syscall ID = %d (%s), arguments = %d, %d, %d, %d, "
+      "return value = %d.",
+      a[0], syscall_names[a[0]], a[1], a[2], a[3], a[4]);
 }
