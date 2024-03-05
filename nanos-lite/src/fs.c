d769daa1a0437837a06bf99cd7dad72b586c4f3e
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-05 14:03:04 +0000

    Feat(nanos): FS open.

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,7 +1,7 @@
 #include <fs.h>
 
-typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
+typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
 typedef struct {
   char *name;
@@ -11,7 +11,7 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB };
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -25,10 +25,21 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
+};
+
+static int file_num = sizeof(file_table) / sizeof(file_table[0]);
+
+int fs_open(const char *pathname, int flags, int mode) {
+  for (int i = 0; i < file_num; i++) {
+    if (strcmp(pathname, file_table[i].name) == 0) {
+      return i;
+    }
+  }
+  return -1;
 };
 
 void init_fs() {
