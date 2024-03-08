8018c175bd62c13345ecf52805482369c28d4b74
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-08 02:45:31 +0000

    Feat(nanos): Implement virtual frame buffer device file.

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,9 +29,26 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   return strlen(buf);
 }
 
-size_t dispinfo_read(void *buf, size_t offset, size_t len) { return 0; }
+size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  uint32_t size =
+      (io_read(AM_GPU_CONFIG).width << 16) + io_read(AM_GPU_CONFIG).height;
+  memcpy(buf, &size, 4); // sizeof(uint32_t) == 4
 
-size_t fb_write(const void *buf, size_t offset, size_t len) { return 0; }
+  return 4; // sizeof(uint32_t) == 4
+}
+
+// buf[0] = x; buf[1] = y; buf[2] = w; buf[3] = h; buf[4] = (uint32_t)pixels;
+size_t fb_write(const void *buf, size_t offset, size_t len) {
+
+  int x = ((uint32_t *)buf)[0], y = ((uint32_t *)buf)[1];
+  int w = ((uint32_t *)buf)[2], h = ((uint32_t *)buf)[3];
+  uint32_t *pixels = (uint32_t *)((uint32_t *)buf)[4];
+
+  io_write(AM_GPU_FBDRAW, x * w, y * h, pixels, w, h, false);
+  io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
+
+  return 0;
+}
 
 void init_device() {
   Log("Initializing devices...");
