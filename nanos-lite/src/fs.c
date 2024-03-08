4bf4c560328f08bfc8ed33615415116cee1a9fc1
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-08 02:43:21 +0000

    Fix(nanos): Read normal file overflow.

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -63,6 +63,10 @@ size_t fs_read(int fd, void *buf, size_t len) {
   if (fd < 0 || fd >= file_num) {
     return 0;
   }
+  if (file_table[fd].read == ramdisk_read &&
+      file_table[fd].open_offset + len > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
   int target_offset = file_table[fd].disk_offset + file_table[fd].open_offset;
   size_t ret = file_table[fd].read(buf, target_offset, len);
   file_table[fd].open_offset += ret;
