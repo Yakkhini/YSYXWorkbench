5cbb8c4d6ad526423ddf5ce6fd5828b1dd5b46dc
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-08 09:52:18 +0000

    Feat(navy): Implement fixedptc API.

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -127,35 +127,39 @@ typedef	__uint128_t fixedptud;
 
 /* Multiplies a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_muli(fixedpt A, int B) {
-	return 0;
+	// A * B = a * 2^8 * b = (a * b) * 2^8
+	return A * B;
 }
 
 /* Divides a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_divi(fixedpt A, int B) {
-	return 0;
+	// A / B = a * 2^8 / b = (a / b) * 2^8
+	return A / B;
 }
 
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
-	return 0;
+	// A * B = a * 2^8 * b * 2^8 = (a * b) * 2^16 != (a * b) * 2^8
+	return A * B / FIXEDPT_ONE;
 }
 
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-	return 0;
+	// A / B = a * 2^8 / b * 2^8 = (a / b) != (a / b) * 2^8
+	return A * FIXEDPT_ONE / B;
 }
 
 static inline fixedpt fixedpt_abs(fixedpt A) {
-	return 0;
+	return (A < 0) ? -A : A;
 }
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
-	return 0;
+	return (A & FIXEDPT_FMASK) ? (A & ~FIXEDPT_FMASK) : A;
 }
 
 static inline fixedpt fixedpt_ceil(fixedpt A) {
-	return 0;
+	return (A & FIXEDPT_FMASK) ? ((A + FIXEDPT_ONE) & ~FIXEDPT_FMASK) : A;
 }
 
 /*
