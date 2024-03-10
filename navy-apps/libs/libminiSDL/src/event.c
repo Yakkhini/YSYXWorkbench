da4c9eac30607f0b3ea09efe5f90119d55aa3193
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-10 08:17:34 +0000

    Feat(navy): Implement keyboard event waiting function in miniSDL.

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -12,11 +13,46 @@ int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
-
 int SDL_WaitEvent(SDL_Event *event) {
+
+  char buf[64];
+  while (1) {
+    if (NDL_PollEvent(buf, sizeof(buf))) {
+      break;
+    }
+  }
+
+  switch (buf[1]) {
+  case 'd':
+    event->type = SDL_KEYDOWN;
+    break;
+  case 'u':
+    event->type = SDL_KEYUP;
+    break;
+  default:
+    return 0;
+  }
+
+  int i = 0;
+  while (1) {
+    if (buf[i] != '\n') {
+      i++;
+    } else {
+      buf[i] = '\0';
+      break;
+    }
+  }
+
+  i = 0;
+  while (i < 81) {
+    if (strcmp(buf + 3, keyname[i]) == 0) {
+      event->key.keysym.sym = i;
+      break;
+    }
+
+    i++;
+  }
+
   return 1;
 }
 
