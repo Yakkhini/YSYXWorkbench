d0821fcf1b4b8624a793187e614b51b31ae70b33
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-12 02:22:46 +0000

    Feat(navy): Implement IMG load function in SDL image lib.

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -1,18 +1,31 @@
-#define SDL_malloc  malloc
-#define SDL_free    free
+#define SDL_malloc malloc
+#define SDL_free free
 #define SDL_realloc realloc
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
+#include <stdio.h>
 
-SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
+SDL_Surface *IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(src->type == RW_TYPE_MEM);
   assert(freesrc == 0);
   return NULL;
 }
 
-SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+SDL_Surface *IMG_Load(const char *filename) {
+  FILE *img_file = fopen(filename, "r");
+  fseek(img_file, 0, SEEK_END);
+  uint32_t size = ftell(img_file);
+  fseek(img_file, 0, SEEK_SET);
+  void *buf = SDL_malloc(size);
+  fread(buf, 1, size, img_file);
+  SDL_Surface *surface = STBIMG_LoadFromMemory(buf, size);
+  if (surface == NULL) {
+    printf("STBIMG_LoadFromMemory failed: %s\n", SDL_GetError());
+  }
+  SDL_free(buf);
+  fclose(img_file);
+  return surface;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
