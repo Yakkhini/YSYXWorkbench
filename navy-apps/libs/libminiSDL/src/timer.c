b6ad07115a9867974c25d6fc96e973a8addb2a94
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-03-15 03:26:02 +0000

    Feat(navy): Implement time delay function in SDL lib.

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -1,14 +1,14 @@
 #include <NDL.h>
 #include <sdl-timer.h>
+#include <stdint.h>
 #include <stdio.h>
 
-SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void *param) {
+SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback,
+                         void *param) {
   return NULL;
 }
 
-int SDL_RemoveTimer(SDL_TimerID id) {
-  return 1;
-}
+int SDL_RemoveTimer(SDL_TimerID id) { return 1; }
 
 uint32_t SDL_GetTicks() {
   // It should be reset or wrap every 49 days according to the doc.
@@ -17,4 +17,7 @@ uint32_t SDL_GetTicks() {
 }
 
 void SDL_Delay(uint32_t ms) {
+  uint32_t start = SDL_GetTicks();
+  while (SDL_GetTicks() - start < ms)
+    ;
 }
