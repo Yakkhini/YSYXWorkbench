66480912a067cb950be38d443282b7a9d185a2aa
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-04-22 22:57:46 +0800

    Style(npc-chisel): IFU built-in code format.

--- npc-chisel/src/main/scala/IFU.scala
+++ npc-chisel/src/main/scala/IFU.scala
@@ -13,20 +13,20 @@ class IFU extends BlackBox with HasBlackBoxInline {
     "IFU.sv",
     """
     |module IFU(
-    | input bit reset,
-    | input [31:0] pc,
-    | output bit [31:0] inst
+    |  input bit reset,
+    |  input [31:0] pc,
+    |  output bit [31:0] inst
     |);
     |
-    | import "DPI-C" function int inst_fetch(int pc);
+    |  import "DPI-C" function int inst_fetch(int pc);
     |
-    | always_comb begin
-    |   if(reset) begin
-    |     inst = 0;
-    |   end else begin
-    |     inst = inst_fetch(pc);
-    |   end
-    | end
+    |  always_comb begin
+    |    if(reset) begin
+    |      inst = 0;
+    |    end else begin
+    |      inst = inst_fetch(pc);
+    |    end
+    |  end
     |endmodule
     |
     |""".stripMargin
