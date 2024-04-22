ae253415d3a2973bd2d0b82591f23e1f3438fae6
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-04-22 22:56:05 +0800

    Feat(npc-chisel): Use bundles in register file.

--- npc-chisel/src/main/scala/RegisterFile.scala
+++ npc-chisel/src/main/scala/RegisterFile.scala
@@ -1,24 +1,19 @@
 package taohe
 
-import chisel3._, chisel3.util._
+import chisel3._
+
+import taohe.util.RegisterFileBundle
+import chisel3.probe.Probe
 
 class RegisterFile extends Module {
-  val io = IO(new Bundle {
-    val readAddr1 = Input(UInt(5.W))
-    val readAddr2 = Input(UInt(5.W))
-    val writeAddr = Input(UInt(5.W))
-    val writeData = Input(UInt(32.W))
-    val writeEnable = Input(Bool())
-    val readData1 = Output(UInt(32.W))
-    val readData2 = Output(UInt(32.W))
-  })
+  val io = IO(new RegisterFileBundle)
 
   val registers = RegInit(VecInit(Seq.fill(32)(0.U(32.W))))
 
-  when(io.writeEnable) {
-    registers(io.writeAddr) := io.writeData
+  when(io.withEXU.writeEnable && io.fromIDU.writeAddr =/= 0.U) {
+    registers(io.fromIDU.writeAddr) := io.withEXU.writeData
   }
 
-  io.readData1 := registers(io.readAddr1)
-  io.readData2 := registers(io.readAddr2)
+  io.withEXU.readData1 := registers(io.fromIDU.readAddr1)
+  io.withEXU.readData2 := registers(io.fromIDU.readAddr2)
 }
