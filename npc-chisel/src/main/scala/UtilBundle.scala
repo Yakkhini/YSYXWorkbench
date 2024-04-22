c4706afba5f76dca4d652ad5634f65bbcd1db4e4
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-04-22 22:53:15 +0800

    Feat(npc-chisel): Useful Bundles.

---
+++ npc-chisel/src/main/scala/UtilBundle.scala
@@ -0,0 +1,45 @@
+package taohe.util
+
+import chisel3._
+import taohe.idu.{ALUOpType, Data1Type, Data2Type, ImmType, InstType}
+
+class RegisterFileBundle extends Bundle {
+  val fromIDU = Flipped(new ControlSignalToRegisterFileBundle)
+  val withEXU = new RegisterFileEXUBundle
+}
+
+class ControlSignalToRegisterFileBundle extends Bundle {
+  val readAddr1 = Output(UInt(5.W))
+  val readAddr2 = Output(UInt(5.W))
+  val writeAddr = Output(UInt(5.W))
+}
+
+class ControlSignalToEXUBundle extends Bundle {
+  val data1Type = Output(UInt(Data1Type.getWidth.W))
+  val data2Type = Output(UInt(Data2Type.getWidth.W))
+  val aluOp = Output(UInt(ALUOpType.getWidth.W))
+  val imm = Output(UInt(32.W))
+}
+
+class ControlSignalBundle extends Bundle {
+  val toRegisterFile = new ControlSignalToRegisterFileBundle
+  val toEXU = new ControlSignalToEXUBundle
+}
+
+class RegisterFileEXUBundle extends Bundle {
+  val writeData = Input(UInt(32.W))
+  val writeEnable = Input(Bool())
+  val readData1 = Output(UInt(32.W))
+  val readData2 = Output(UInt(32.W))
+}
+
+class IDUBundle extends Bundle {
+  val inst = Input(UInt(32.W))
+  val controlSignal = new ControlSignalBundle
+}
+
+class EXUBundle extends Bundle {
+  val currentPC = Input(UInt(32.W))
+  val fromIDU = Flipped(new ControlSignalToEXUBundle)
+  val withRegisterFile = Flipped(new RegisterFileEXUBundle)
+}
