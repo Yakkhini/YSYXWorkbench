9330214f965d6f4d4dd9fc40c98873760e8cfaf4
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-04-23 17:24:59 +0800

    Fix(npc-chisel): Extend sign in imm.

--- npc-chisel/src/main/scala/IDU/IDU.scala
+++ npc-chisel/src/main/scala/IDU/IDU.scala
@@ -5,6 +5,7 @@ import chisel3.util.MuxLookup
 import taohe.idu.ImmType.S
 import taohe.idu.ImmType.U
 import taohe.util.IDUBundle
+import chisel3.util.Fill
 
 class IDU extends Module {
   val io = IO(new IDUBundle)
@@ -13,13 +14,16 @@ class IDU extends Module {
 
   val decodeResult = decodeTable.decode(io.inst)
 
-  val imm_i = io.inst(31) ## 0.U(20.W) ## io.inst(30, 20)
-  val imm_s = io.inst(31) ## 0.U(20.W) ## io.inst(30, 25) ## io.inst(11, 7)
-  val imm_b = io.inst(31) ## 0.U(19.W) ## io.inst(7) ## io.inst(30, 25) ## io
-    .inst(11, 8) ## 0.U(1.W)
+  val imm_i = io.inst(31) ## Fill(20, io.inst(31)) ## io.inst(30, 20)
+  val imm_s =
+    io.inst(31) ## Fill(20, io.inst(31)) ## io.inst(30, 25) ## io.inst(11, 7)
+  val imm_b =
+    io.inst(31) ## Fill(19, io.inst(31)) ## io.inst(7) ## io.inst(30, 25) ## io
+      .inst(11, 8) ## 0.U(1.W)
   val imm_u = io.inst(31, 12) ## 0.U(12.W)
-  val imm_j = io.inst(31) ## 0.U(10.W) ## io.inst(19, 12) ## io.inst(20) ## io
-    .inst(30, 21) ## 0.U(1.W)
+  val imm_j =
+    io.inst(31) ## Fill(10, io.inst(31)) ## io.inst(19, 12) ## io.inst(20) ## io
+      .inst(30, 21) ## 0.U(1.W)
 
   val immType = decodeResult(ImmField)
 
