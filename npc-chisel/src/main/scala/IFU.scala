82a321fc102f01c1f824705742d8201179ddfbd9
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-05-15 21:48:01 +0800

    Feat(npc-chisel): IFU valid depends on SRAM.

--- npc-chisel/src/main/scala/IFU.scala
+++ npc-chisel/src/main/scala/IFU.scala
@@ -4,23 +4,28 @@ import chisel3._
 import chisel3.util.HasBlackBoxInline
 
 import taohe.util.IFUBundle
+import taohe.util.enum.UnitState
+import chisel3.util.{switch, is}
 
 class IFU extends Module {
   val io = IO(new IFUBundle)
-
-  io.toIDU.valid := false.B
-  io.fromEXU.ready := false.B
 
   val pc = RegInit("h80000000".U(32.W))
   val inst = Wire(UInt(32.W))
 
   val sram = Module(new SRAM())
+  val sramValid = Wire(Bool())
+
+  // Default value
+  io.toIDU.valid := sramValid
+  io.fromEXU.ready := false.B
+
+  pc := Mux(io.fromEXU.valid, io.fromEXU.bits.nextPC, pc)
 
   sram.io.readAddr := pc
+  sramValid := sram.io.valid
   inst := sram.io.readData
 
   io.toIDU.bits.currentPC := pc
   io.toIDU.bits.inst := inst
-
-  pc := io.fromEXU.bits.nextPC
 }
--- npc-chisel/src/main/scala/SRAM.scala
+++ npc-chisel/src/main/scala/SRAM.scala
@@ -3,12 +3,19 @@ package taohe
 import chisel3._
 
 import taohe.dpic.MemRead
+import chisel3.util.{switch, is}
 
 class SRAM extends Module {
   val io = IO(new Bundle {
     val readAddr = Input(UInt(32.W))
     val readData = Output(UInt(32.W))
+    val valid = Output(Bool())
   })
+
+  val addr = RegInit(0.U(32.W))
+  addr := io.readAddr
+
+  io.valid := io.readAddr === addr
 
   val memRead = Module(new MemRead())
 
