055019bb960f8b2b15e6a4a0327911f0a897d1d2
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2024-07-26 21:12:16 +0800

    Feat(npc-chisel): AXI-Lite interface Arbiter.

--- npc-chisel/src/main/scala/SRAM/SRAMArbiter.scala
+++ npc-chisel/src/main/scala/SRAM/SRAMArbiter.scala
@@ -1,12 +1,13 @@
 package taohe
 
 import chisel3._
-import taohe.util.SRAMBundle
+import taohe.util.AXI4LiteBundle
+import chisel3.util.Arbiter
 import chisel3.util.{switch, is}
 
 class SRAMArbiter extends Module {
-  val ifuIO = IO(new SRAMBundle)
-  val lsuIO = IO(new SRAMBundle)
+  val ifuIO = IO(Flipped(new AXI4LiteBundle))
+  val lsuIO = IO(Flipped(new AXI4LiteBundle))
 
   val sram1 = Module(new SRAM)
   val sram2 = Module(new SRAM)
