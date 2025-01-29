28ca6613064afe1e99eb6869a43dbde502334640
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-01-29 12:28:23 +0800

    [am] derive ysyxsoc compile target from npc target

---
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -0,0 +1,26 @@
+AM_SRCS := riscv/ysyxsoc/start.S \
+           riscv/ysyxsoc/trm.c \
+           riscv/ysyxsoc/ioe.c \
+           riscv/ysyxsoc/timer.c \
+           riscv/ysyxsoc/input.c \
+           riscv/ysyxsoc/cte.c \
+           riscv/ysyxsoc/trap.S \
+           platform/dummy/vme.c \
+           platform/dummy/mpe.c
+
+CFLAGS    += -fdata-sections -ffunction-sections
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
+						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --gc-sections -e _start
+CFLAGS += -DMAINARGS=\"$(mainargs)\"
+CFLAGS += -I$(AM_HOME)/am/src/riscv/ysyxsoc/include
+.PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
+
+image: $(IMAGE).elf
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+run: image
+	@just -f $(NPC_CHISEL)/Justfile -d $(NPC_CHISEL) sim
+	@taohe -f $(IMAGE).elf $(IMAGE).bin
