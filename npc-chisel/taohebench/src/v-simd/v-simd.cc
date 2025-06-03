c299e2356e7dbb316769779fc131e813a82967b9
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-06-03 13:40:30 +0800

    [taohebench] define vsimd device structure

--- npc-chisel/taohebench/src/v-simd/v-simd.cc
+++ npc-chisel/taohebench/src/v-simd/v-simd.cc
@@ -1,9 +1,30 @@
 #include <v-simd/v-simd.h>
 
 #define DEVICE_BASE 0xa2000000
-#define DEVICE_SIZE 0x1000000
 
-static uint8_t VSIMD_MEM[DEVICE_SIZE] __attribute((aligned(4096))) = {};
+enum VSIMDState {
+  VSIMD_IDLE = 0x00,
+  VSIMD_SETZERO = 0x01,
+  VSIMD_LOADDUP = 0x02,
+  VSIMD_LOAD = 0x03,
+  VSIMD_MUL_ADD = 0x04,
+};
+
+typedef struct {
+  enum VSIMDState state;
+  uintptr_t ptr[3];
+  fixedpt vreg[3];
+} VSIMD;
+
+VSIMD vsimd;
+
+void vsimd_init() {
+  vsimd.state = VSIMD_IDLE;
+  for (int i = 0; i < 3; i++) {
+    vsimd.ptr[i] = fixedpt_fromint(0);
+    vsimd.vreg[i] = fixedpt_fromint(0);
+  }
+}
 
 void vsimd_receiver(paddr_t addr, int len, word_t data) {
   Log("Not Implemented Yet");
