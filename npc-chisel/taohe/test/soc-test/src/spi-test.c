79921bca43c48e7132de01640409fc692eb86e3b
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-06-23 03:46:08 +0800

    [taohe] use SPI interface in spi-test

--- npc-chisel/taohe/test/soc-test/src/spi-test.c
+++ npc-chisel/taohe/test/soc-test/src/spi-test.c
@@ -11,18 +11,7 @@ void spi_test_case(uint32_t input, uint32_t expect);
 void spi_test() {
   printf("SPI Test Start\n");
 
-  uint32_t *spi_base = (uint32_t *)0x10001000;
-  uint8_t *spi_ctrl = (uint8_t *)(spi_base + 4);
-  uint32_t *spi_divider = spi_base + 5;
-  uint8_t *spi_ss = (uint8_t *)(spi_base + 6);
-
-  outl((uintptr_t)(spi_base + 4), 0x00000000); // reset
-  outb((uintptr_t)spi_ctrl, 0B00001000);       // set CHARLEN to 8
-  outb((uintptr_t)(spi_ctrl + 1),
-       0B00000010); // LSB=0, TxNEG=0, RxNEG=1, GOBSY=0
-
-  outl((uintptr_t)spi_divider, 0x10000000); // set divider to 10000000
-  outl((uintptr_t)spi_ss, 0B10000000);      // set SS to seven bit
+  spi_device_active(SPI_BITREV_ID);
 
   spi_test_case(0x0F, 0xF0);
   spi_test_case(0xF0, 0x0F);
@@ -33,26 +22,9 @@ void spi_test() {
 }
 
 void spi_test_case(uint32_t input, uint32_t expect) {
-  uint32_t *spi_base = (uint32_t *)0x10001000;
-  uint8_t *spi_ctrl = (uint8_t *)(spi_base + 4);
 
-  // First Transfer: send data to the slave
-  while ((inb((uintptr_t)(spi_ctrl + 1)) & 0B00000001) == 1)
-    ;
-  outl((uintptr_t)spi_base, input); // send 0x00
-  outb((uintptr_t)(spi_ctrl + 1),
-       0B00000011); // LSB=0, TxNEG=0, RxNEG=1, GOBSY=1
-
-  // Second Transfer: receive data from the slave
-  while ((inb((uintptr_t)(spi_ctrl + 1)) & 0B00000001) == 1)
-    ;
-  outb((uintptr_t)(spi_ctrl + 1),
-       0B00000011); // LSB=0, TxNEG=0, RxNEG=1, GOBSY=1
-
-  // Read the data from spi master after second transfer is done
-  while ((inb((uintptr_t)(spi_ctrl + 1)) & 0B00000001) == 1)
-    ;
-  uint32_t data = inw((uintptr_t)spi_base);
+  uint32_t data;
+  data = spi_transfer(input);
 
   // Assert the data is correct
   assert(data == expect);
