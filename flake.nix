cce9d1e4df5a6a36b524ca6cebf22ba51889bedb
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-06-29 22:45:10 +0800

    [project] add coursier to install scala toolchain

--- flake.nix
+++ flake.nix
@@ -57,6 +57,7 @@
         stdpkgs.yosys-synlig
         stdpkgs.surelog
         stdpkgs.verible
+        stdpkgs.coursier
         stdpkgs.metals # scala lsp
         stdpkgs.ieda
         stdpkgs.capstone
