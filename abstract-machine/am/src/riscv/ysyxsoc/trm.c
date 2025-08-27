7dab8d0fee80103288afdb76171afde4c9a15920
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-08-27 20:46:31 +0800

    [am] use correct symbol to define heap area

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -3,14 +3,10 @@
 #include <klib.h>
 #include <ysyxsoc.h>
 
-extern char _heap_start;
+extern char _heap_start, _stack_pointer;
 int main(const char *args);
 
-extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
-#define PMEM_END ((uintptr_t)&_pmem_start + PMEM_SIZE)
-
-Area heap = RANGE(&_heap_start, PMEM_END);
+Area heap = RANGE(&_heap_start, &_stack_pointer);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
