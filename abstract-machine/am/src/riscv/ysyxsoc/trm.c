820476b9ad92e038a939c6295219c6ba83b492c2
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2025-09-18 23:55:00 +0800

    [am] load all text and data into pasram and use sram as stack to support larger program

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,9 +1,10 @@
 #include <am.h>
 #include <klib-macros.h>
 #include <klib.h>
+#include <stdint.h>
 #include <ysyxsoc.h>
 
-extern char _heap_start, _stack_pointer;
+extern char _heap_start, _heap_end;
 int main(const char *args);
 void _trm_init();
 
@@ -38,6 +39,7 @@ void *bootloader_memcpy(void *out, const void *in, size_t n) {
 
 extern char _psram_start, _rwdata_load_start, _rwdata_load_end;
 extern char _sram_start, _rxdata_load_start, _rxdata_load_end;
+extern char _rxdate_dest_start, _rwdata_dest_start;
 
 __attribute__ ((section (".bootloader")))
 void bootloader() {
@@ -45,11 +47,11 @@ void bootloader() {
   // Bootloader
   uint32_t rxdata_size = (uintptr_t)&_rxdata_load_end - (uintptr_t)&_rxdata_load_start;
   uint32_t rwdata_size = (uintptr_t)&_rwdata_load_end - (uintptr_t)&_rwdata_load_start;
-  bootloader_memcpy(&_sram_start, &_rxdata_load_start, rxdata_size);
-  bootloader_memcpy(&_psram_start, &_rwdata_load_start, rwdata_size);
+  bootloader_memcpy(&_rxdate_dest_start, &_rxdata_load_start, rxdata_size);
+  bootloader_memcpy(&_rwdata_dest_start, &_rwdata_load_start, rwdata_size);
 
   heap.start = &_heap_start;
-  heap.end = &_stack_pointer;
+  heap.end = &_heap_end;
 
   _trm_init();
 
@@ -68,9 +70,9 @@ void _trm_init() {
   uint32_t rxdata_size = (uintptr_t)&_rxdata_load_end - (uintptr_t)&_rxdata_load_start;
   uint32_t rwdata_size = (uintptr_t)&_rwdata_load_end - (uintptr_t)&_rwdata_load_start;
   printf("RX Bootload Finish. Source start address: 0x%08X, Source end address: 0x%08X, Dest start address: 0x%08X, size: %ld\n",
-         &_rxdata_load_start, &_rxdata_load_end, &_sram_start, rxdata_size);
+         &_rxdata_load_start, &_rxdata_load_end, &_rxdate_dest_start, rxdata_size);
   printf("RW Bootload Finish. Source start address: 0x%08X, Source end address: 0x%08X, Dest start address: 0x%08X, size: %ld\n",
-         &_rwdata_load_start, &_rwdata_load_end, &_psram_start, rwdata_size);
+         &_rwdata_load_start, &_rwdata_load_end, &_rwdata_dest_start, rwdata_size);
   printf("Heap range: [0x%08X, 0x%08X)\n", heap.start, heap.end);
 
   int ret = main(mainargs);
