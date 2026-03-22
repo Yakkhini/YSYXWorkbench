5726921950a5ec2df4c157c33f4dc6f6b5f37615
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2026-03-22 22:10:22 +0800

    [nanos, navy] handle proc args via SysV ABI stack init convention

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,15 +30,56 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
   pcb->cp = kcontext(area, entry, arg);
 }
 
-void context_uload(PCB *pcb, char *filename) {
-  Area area = {pcb->stack, pcb->stack + STACK_SIZE};
+// Ref: Process stack initialization in LSB 5.0.0 AMD64 core refspec,
+// [https://refspecs.linuxfoundation.org/LSB_5.0.0/LSB-Core-AMD64/LSB-Core-AMD64.html].
+void context_uload(PCB *pcb, char *filename, char *argv[], char *envp[]) {
+  uint32_t argc = 0;
+  uint32_t envc = 0;
 
-  uint8_t *ustack_start = heap.end - user_stack_alloc_offset - STACK_SIZE;
+  uint8_t *ustack_end = heap.end - user_stack_alloc_offset;
   user_stack_alloc_offset += STACK_SIZE;
+
+  uint8_t *stack_pointer = ustack_end - 0x1000;
+  uint8_t *arg_string_pointer = stack_pointer + 0x500;
+
+  for (int i = 0; argv[i] != NULL; i++) {
+    argc++;
+    *(uintptr_t *)(stack_pointer + (i + 1) * sizeof(uintptr_t)) =
+        (uintptr_t)arg_string_pointer;
+
+    size_t arg_len = strlen(argv[i]) + 1;
+    memcpy(arg_string_pointer, argv[i], arg_len);
+    argv[i] = (char *)arg_string_pointer;
+    arg_string_pointer += arg_len;
+  }
+
+  *(uintptr_t *)(stack_pointer + (argc + 2) * sizeof(uintptr_t)) =
+      (uintptr_t)NULL;
+
+  for (int i = 0; envp[i] != NULL; i++) {
+    envc++;
+    *(uintptr_t *)(stack_pointer + (argc + 2 + i) * sizeof(uintptr_t)) =
+        (uintptr_t)arg_string_pointer;
+
+    size_t env_len = strlen(envp[i]) + 1;
+    memcpy(arg_string_pointer, envp[i], env_len);
+    envp[i] = (char *)arg_string_pointer;
+    arg_string_pointer += env_len;
+  }
+
+  *(uintptr_t *)(stack_pointer + (argc + 2 + envc + 1) * sizeof(uintptr_t)) =
+      (uintptr_t)NULL;
+
+  *(uintptr_t *)stack_pointer = argc;
+
+  Log("Loading program '%s' with argc = %d, envc = %d, stack_pointer = %p",
+      filename, argc, envc, stack_pointer);
+
+  Area area = {pcb->stack, pcb->stack + STACK_SIZE};
 
   Context *c = (Context *)(area.end - sizeof(Context));
   memset(c, 0, sizeof(Context));
-  c->GPR2 = (uintptr_t)ustack_start;
+  c->GPR2 = (uintptr_t)stack_pointer;
 
   void(*entry) = (void (*)())loader(pcb, filename);
 
@@ -50,7 +91,8 @@ void init_proc() {
   Log("Initializing processes...");
 
   context_kload(&pcb[0], hello_fun, (void *)1L);
-  context_uload(&pcb[1], "/bin/pal");
+  context_uload(&pcb[1], "/bin/pal", (char *[]){"--skip", NULL},
+                (char *[]){NULL});
   switch_boot_pcb();
 }
 
