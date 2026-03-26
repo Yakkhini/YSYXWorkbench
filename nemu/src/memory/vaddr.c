df2db3865c039e92e47797c06b7383b1619b524d
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2026-03-26 20:02:04 +0800

    [nemu] implement MMU translation

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -1,29 +1,47 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ * PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 #include <memory/paddr.h>
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if (isa_mmu_check(addr, len, MEM_TYPE_READ) == MMU_DIRECT) {
+    return paddr_read(addr, len);
+  }
+  paddr_t addr_translated = isa_mmu_translate(addr, len, MEM_TYPE_READ);
+
+  // Currently all va -> pa directly mapped so use this assert.
+  Assert(addr == addr_translated, "Address translation not equall.");
+  return paddr_read(addr_translated, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if (isa_mmu_check(addr, len, MEM_TYPE_READ) == MMU_DIRECT) {
+    return paddr_read(addr, len);
+  }
+  paddr_t addr_translated = isa_mmu_translate(addr, len, MEM_TYPE_READ);
+  Assert(addr == addr_translated, "Address translation not equall.");
+  return paddr_read(addr_translated, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  paddr_write(addr, len, data);
+  if (isa_mmu_check(addr, len, MEM_TYPE_WRITE) == MMU_DIRECT) {
+    paddr_write(addr, len, data);
+    return;
+  }
+  paddr_t addr_translated = isa_mmu_translate(addr, len, MEM_TYPE_WRITE);
+  Assert(addr == addr_translated, "Address translation not equall.");
+  paddr_write(addr_translated, len, data);
 }
