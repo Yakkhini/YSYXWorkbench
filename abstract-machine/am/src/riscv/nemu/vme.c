1ce05e4bbab159710f02dc07214fe0b29af119b0
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2026-03-30 16:35:32 +0800

    [am] not switch kernel addr spcace to avoid stack error
    The kernel addr space is mapped in every user addr space so it's fine.

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -55,7 +55,9 @@ void protect(AddrSpace *as) {
 void unprotect(AddrSpace *as) {}
 
 void __am_get_cur_as(Context *c) {
-  c->pdir = (vme_enable ? (void *)get_satp() : NULL);
+  if (c->pdir != NULL) {
+    c->pdir = (vme_enable ? (void *)get_satp() : NULL);
+  }
 }
 
 void __am_switch(Context *c) {
