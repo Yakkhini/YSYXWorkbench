4f05bf124bbf61db85092602f95c52ffd756ecdb
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2026-03-31 20:59:58 +0800

    [nemu] use fixed witdth when print register info

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ * PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include "local-include/reg.h"
 #include <isa.h>
@@ -24,18 +24,18 @@ const char *regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
                       "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
 void isa_reg_display() {
-  printf("pc 0x%X\n", cpu.pc);
+  printf("pc 0x%08X\n", cpu.pc);
   printf("Print gpr...\n");
-  printf("zero 0x%X;\n", cpu.gpr[0]);
-  printf("%s 0x%X;   ", regs[1], cpu.gpr[1]);
-  printf("%s 0x%X;   ", regs[2], cpu.gpr[2]);
-  printf("%s 0x%X;\n", regs[3], cpu.gpr[3]);
+  printf("zero 0x%08X;\n", cpu.gpr[0]);
+  printf("%s 0x%08X;   ", regs[1], cpu.gpr[1]);
+  printf("%s 0x%08X;   ", regs[2], cpu.gpr[2]);
+  printf("%s 0x%08X;\n", regs[3], cpu.gpr[3]);
 
   for (int i = 4; i < 29; i += 4) {
-    printf("%s 0x%X;   ", regs[i], cpu.gpr[i]);
-    printf("%s 0x%X;   ", regs[i + 1], cpu.gpr[i + 1]);
-    printf("%s 0x%X;   ", regs[i + 2], cpu.gpr[i + 2]);
-    printf("%s 0x%X;\n", regs[i + 3], cpu.gpr[i + 3]);
+    printf("%s 0x%08X;   ", regs[i], cpu.gpr[i]);
+    printf("%s 0x%08X;   ", regs[i + 1], cpu.gpr[i + 1]);
+    printf("%s 0x%08X;   ", regs[i + 2], cpu.gpr[i + 2]);
+    printf("%s 0x%08X;\n", regs[i + 3], cpu.gpr[i + 3]);
   }
 }
 
