64b2cd4ba71a9631f590e4088d6ce29d13042462
Yaksis <59007159+Yakkhini@users.noreply.github.com>
2026-06-11 19:13:43 +0800

    [am] add `riscv32im-ysyxsoc` build target

---
+++ abstract-machine/scripts/riscv32im-ysyxsoc.mk
@@ -0,0 +1,6 @@
+include $(AM_HOME)/scripts/isa/riscv.mk
+include $(AM_HOME)/scripts/platform/ysyxsoc.mk
+CFLAGS  += -DISA_H=\"riscv/riscv.h\"
+COMMON_CFLAGS += -march=rv32im_zicsr_zifencei -mabi=ilp32e  # overwrite
+LDFLAGS       += -melf32lriscv                    # overwrite
+
